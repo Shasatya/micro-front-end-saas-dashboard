1007604d4e9a2affd1e8cf528a5911e99adec6d2
Shasatya <120493084+Shasatya@users.noreply.github.com>
2026-01-15 21:16:39 +0530

    FEAT: added pg and prisma adapter for database

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,1 +1,23 @@
+import { PrismaClient } from "@prisma/client";
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+
 export * from "@prisma/client";
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+const createPrismaClient = () => {
+  const connectionString = process.env.DATABASE_URL;
+  const pool = new Pool({ connectionString });
+
+  const adapter = new PrismaPg(pool);
+
+  return new PrismaClient({
+    adapter,
+    log: ["query"],
+  });
+};
+
+export const prisma = globalForPrisma.prisma || createPrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
